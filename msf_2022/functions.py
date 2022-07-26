11fb8650c10f830b36ac73561e398336b16ac31f
melisa <alkan@iastate.edu>
2022-07-26 13:13:30 -0500

    add tests

--- msf_2022/functions.py
+++ msf_2022/functions.py
@@ -38,24 +38,24 @@ if __name__ == "__main__":
 
 def calculate_distance(rA: np.ndarray, rB: np.ndarray) -> float:
     """
-	Calculate the distance between two points
+    Calculate the distance between two points
 
-	Parameters
-	----------
-	rA, rB : np.ndarray
-		The coordinates of each point
-	
-	Returns
-	-------
-	dist: np.floating
-		The distance between the two points
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+            The coordinates of each point
 
-	Examples
-	--------
-	>>> r1 = np.array([0,0,0])
-	>>> r2 = np.array([0,1.0,0])
-	>>> calculate_distance(r1,r2)
-	1.0
+    Returns
+    -------
+    dist: np.floating
+            The distance between the two points
+
+    Examples
+    --------
+    >>> r1 = np.array([0,0,0])
+    >>> r2 = np.array([0,1.0,0])
+    >>> calculate_distance(r1,r2)
+    1.0
     """
     # This function calculates the distance between two points given as numpy arrays.
     d = rA - rB
@@ -201,7 +201,8 @@ def bond_histogram(bond_list, save_location=None, dpi=300, graph_min=0, graph_ma
 
 
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
-
+    # 	if min_bond < 0:
+    # 		raise ValueError("Minimum bond length must be greater to")
     # Find the bonds in a molecule (set of coordinates) based on distance criteria.
     bonds = {}
     num_atoms = len(coordinates)
